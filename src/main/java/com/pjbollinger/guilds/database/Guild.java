d28d8af4923fe1be1de8bebe69745391cbbd392b
Patrick Bollinger <pjbollinger@student.ysu.edu>
2013-10-12 20:30:39 -0400

    Add Google GSON for future JSON implementation.
    Added classes that will make up the database

---
+++ src/main/java/com/pjbollinger/guilds/database/Guild.java
@@ -0,0 +1,5 @@
+package com.pjbollinger.guilds.database;
+
+public class Guild {
+     //TODO Add everything that makes a guild a guild
+}
---
+++ src/main/java/com/pjbollinger/guilds/database/Player.java
@@ -0,0 +1,5 @@
+package com.pjbollinger.guilds.database;
+
+public class Player {
+    //TODO Add everything that makes a player a player
+}

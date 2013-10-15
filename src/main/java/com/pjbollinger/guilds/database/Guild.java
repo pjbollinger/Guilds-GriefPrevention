c42e0e1a157047097bb53c875f658158da2222bb
Patrick Bollinger <pjbollinger@student.ysu.edu>
2013-10-15 06:47:03 -0400

    Started to work on Guild class for the database

--- src/main/java/com/pjbollinger/guilds/database/Guild.java
+++ src/main/java/com/pjbollinger/guilds/database/Guild.java
@@ -1,5 +1,44 @@
 package com.pjbollinger.guilds.database;
 
+import java.util.List;
+
 public class Guild {
-     //TODO Add everything that makes a guild a guild
+
+    private String name;
+    private String leader;
+    private List<String> moderators;
+    private List<String> members;
+    private List<Guild> alliances;
+    private int pool;
+    private int claimed;
+    private int[] claims;
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLeader() {
+        return this.leader;
+    }
+
+    public void setLeader(String leader) {
+        this.leader = leader;
+    }
+
+    public List<String> getModerators() {
+        return this.moderators;
+    }
+
+    public void setModerators(List<String> moderators) {
+        this.moderators = moderators;
+    }
+
+    public void addModerator(String moderator) {
+        this.moderators.add(moderator);
+    }
+
 }
